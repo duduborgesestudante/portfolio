37e99947a9ccb50540ec81e3b17f6c1843fa5675
EDUARDO BORGES CAMBRAIA <eduardo_cambraia@estudante.sesisenai.org.br>
2023-10-25 11:30:11 -0300

    ta

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function linkd(){
+    window.open("https://www.linkedin.com/in/eduardo-borges-cambraia-809225269/")
+}
+function insta(){
+    window.open("https://www.instagram.com/borges.ip/")
+}
+function github(){
+    window.open("https://github.com/duduborges")
+}
